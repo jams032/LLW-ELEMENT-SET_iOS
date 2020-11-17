6c73d71a8855f108029c96dbf044598cb3b83342
jams032 <jamshed.bas@gmail.com>
2020-11-17 21:26:35 +0600

    Implement element collection view
    Implement element collection view

--- LLW-ELEMENT-SET_iOS/LLW-ELEMENT-SET_iOS/ViewController.swift
+++ LLW-ELEMENT-SET_iOS/LLW-ELEMENT-SET_iOS/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var finalElements: UILabel!
+        
+        var items = [ElementSet]()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            items.append(ElementSet())
+            items.append(ElementSet())
+            items.append(ElementSet())
+            items.append(ElementSet())
+            items.append(ElementSet())
+            items.append(ElementSet())
+            items.append(ElementSet())
+            items.append(ElementSet())
+
+        }
+
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        func formatElementsForDisplay(_ element: ElementSet) -> String {
+            let numbers = element.converged().map { "\($0.value)" }
+            let timeStamps = element.converged().map { "\($0.timestamp)" }
+            let strTimes = timeStamps.joined(separator: ", ")
+            let str = numbers.joined(separator: ", ")
+            
+            var result = zip(str, strTimes).flatMap { [$0.0, $0.1] }
+            
+            return str
+        }
     }
 
+    extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return items.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+            let item = items[indexPath.row]
+            cell.delegate = self
+            cell.elementSet = item
+            let str = formatElementsForDisplay(item)
+            cell.elements.text = str
+            cell.replicaNo.text = "Replica \(indexPath.row+1)"
+            return cell
+        }
+    }
 
-}
+    extension ViewController: CollectionViewCellDelegate {
+        func didChange(elementSet: ElementSet?) {
+            if let index = items.index(where: { $0 === elementSet }) {
+                items[index] = elementSet!
+                collectionView.reloadData()
+                let merged = ElementSet.mergedItems(items)
+                let str = formatElementsForDisplay(merged)
+                finalElements.text = str
+            }
+        }
+    }
 
